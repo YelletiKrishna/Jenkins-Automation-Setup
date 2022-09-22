d621d78588d1bd618332e5a7e30e6d2fd7635581
Yelleti Krishna <krishna.yelleti@publicissapient.com>
2022-09-22 19:20:40 +0530

    Added springboot application

---
+++ src/main/java/com/pipeline/jenkinsautomationsetup/JenkinsAutomationSetupApplication.java
@@ -0,0 +1,21 @@
+package com.pipeline.jenkinsautomationsetup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class JenkinsAutomationSetupApplication {
+
+    @GetMapping
+    public String message(){
+        return "welcome to ChaiWorld";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(JenkinsAutomationSetupApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pipeline/jenkinsautomationsetup/JenkinsAutomationSetupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pipeline.jenkinsautomationsetup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsAutomationSetupApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
